c92009328eaf81f0fc225a05aa22350cd0b0cbb4
nakyung128 <lnk12888@naver.com>
2023-03-24 01:07:43 +0900

    Custom TabBar

---
+++ Chapter03-CSTabBar/ThirdViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThirdViewController.swift
+//  Chapter03-CSTabBar
+//
+//  Created by 이나경 on 2023/03/24.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let label = UILabel(frame: CGRect(x: 0, y: 150, width: 100, height: 30))
+        label.textColor = UIColor.red
+        label.textAlignment = .center
+        label.text = "세 번째 화면"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        label.sizeToFit()
+        label.center.x = self.view.frame.width / 2
+        
+        self.view.addSubview(label)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
